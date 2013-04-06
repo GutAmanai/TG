c1703118c6f30f5a943e835a0d6cb14c43349888
vnl8548 <6ssvEpoRoOvzGOA0AwpJ>
2013-04-06 00:04:42 -0300

    tste git vinicus

---
+++ br.persistencia.tg/Repositorios/IUnidadeDeTrabalho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace br.persistencia.tg.Repositorios
+{
+    public interface IUnidadeDeTrabalho : IDisposable
+    {
+        bool ASessaoEstaAberta { get; }
+        bool ATransacaoEstaAtiva { get; }
+        void Inicializar();
+        void Salvar();
+    }
+}
