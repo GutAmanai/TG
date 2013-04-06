05b8dc9af54c4a1ea5bc8391a74aa9b2b9b205be
GutAmanai <gustavo.amanai@gmail.com>
2013-04-06 20:26:57 -0300

    Teste

--- br.dominio.tg/Entidades/Exemplo.cs
+++ br.dominio.tg/Entidades/Exemplo.cs
@@ -9,7 +9,7 @@ namespace br.dominio.tg.Entidades
     {
         //Teste Gustavo.
         // teste 022
-        // Teste llalalalal
+        // Teste Commit
         public virtual string Nome { get; protected set; }
         public virtual DateTime Data { get; protected set; }
     }
