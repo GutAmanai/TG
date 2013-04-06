18134be9baa355807d7b1f1193c692348c031faa
vnl8548 <6ssvEpoRoOvzGOA0AwpJ>
2013-04-06 00:32:59 -0300

    teste commit Vc

--- br.banco.tg/BancoTeste.cs
+++ br.banco.tg/BancoTeste.cs
@@ -9,7 +9,7 @@ namespace br.banco.tg
         [TestMethod]
         public void gera_script()
         {
-            //teste git   
+            //teste git 2
             //DbHelper.GerarScriptDeCriacaoDoSchema(@"c:\produto-schema-eguru_");
             Assert.IsTrue(true);
         }
