e5a737edc66bbf3c5938ea075226bbb326cf141f
GutAmanai <gustavo.amanai@gmail.com>
2013-04-06 23:55:21 -0300

    Teste!!!

--- br.dominio.tg/Entidades/Exemplo.cs
+++ br.dominio.tg/Entidades/Exemplo.cs
@@ -7,9 +7,7 @@ namespace br.dominio.tg.Entidades
 {
     public class Exemplo : EntidadeBase
     {
-        //Teste Gustavo.
-        // teste 022
-        // Teste Commit 0011
+        //Tese agora foi!!!!
         public virtual string Nome { get; protected set; }
         public virtual DateTime Data { get; protected set; }
     }
