c44e492025b7d9f89828be6f465b00ec9299a8ab
GutAmanai <gustavo.amanai@gmail.com>
2013-07-29 22:52:52 -0300

    Correção do erros e conflitos.

--- br.aplicacao.tg/ViewModel/ViewModelPromocao.cs
+++ br.aplicacao.tg/ViewModel/ViewModelPromocao.cs
@@ -7,14 +7,14 @@ namespace br.aplicacao.tg.ViewModel
 {
     public class ViewModelPromocao
     {
-        public int IdPromocao               { get; set; }
-        public bool Ativo                   { get; set; }
-        public DateTime DataExpiracao       { get; set; }
-        public string Nome                  { get; set; }
-        public DateTime DataEntrada         { get; set; }
-        public string Descricao             { get; set; }
-        public string ImagemUrl             { get; set; }
-        public double Latitude              { get; set; }
-        public double Longitude             { get; set; }
+        public int IdPromocao { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataExpiracao { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataEntrada { get; set; }
+        public string Descricao { get; set; }
+        public string ImagemUrl { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
