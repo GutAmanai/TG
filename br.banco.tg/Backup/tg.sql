647af2b58fc7101c502666c2ff2ba0eb8fd0e3d7
dodsddragon <dhouglas.lb@hotmail.com>
2013-08-20 00:13:13 -0300

    Script alterado.

--- br.banco.tg/Backup/tg.sql
+++ br.banco.tg/Backup/tg.sql
@@ -1,4 +1,4 @@
-﻿# Host: localhost  (Version: 6.0.3-alpha-community)
+# Host: localhost  (Version: 6.0.3-alpha-community)
 # Date: 2013-07-29 23:11:40
 # Generator: MySQL-Front 5.3  (Build 4.4)
 
@@ -21,6 +21,8 @@ CREATE TABLE `cliente` (
   `Contato` varchar(100) NOT NULL,
   `FotoUrl` varchar(100) DEFAULT NULL,
   `Senha` varchar(100) NOT NULL,
+  `Longitude` double NOT NULL,
+  `Latitude` double NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Email` (`Email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -30,25 +32,6 @@ CREATE TABLE `cliente` (
 #
 
 
-#
-# Source for table "consumidor"
-#
-
-DROP TABLE IF EXISTS `consumidor`;
-CREATE TABLE `consumidor` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Nome` varchar(100) NOT NULL,
-  `DataEntrada` datetime NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Contato` varchar(100) NOT NULL,
-  `Senha` varchar(100) NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `Email` (`Email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-#
-# Data for table "consumidor"
-#
 
 
 #
@@ -92,49 +75,5 @@ CREATE TABLE `clientepromocao` (
 
 #
 # Data for table "clientepromocao"
-#
-
-
-#
-# Source for table "usuario"
-#
-
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE `usuario` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Nome` varchar(100) NOT NULL,
-  `DataEntrada` datetime NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Contato` varchar(100) NOT NULL,
-  `Senha` varchar(100) NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `Email` (`Email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-#
-# Data for table "usuario"
-#
-
-
-#
-# Source for table "qualificacaopromocao"
-#
-
-DROP TABLE IF EXISTS `qualificacaopromocao`;
-CREATE TABLE `qualificacaopromocao` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Gostou` tinyint(1) NOT NULL,
-  `DataEntrada` datetime NOT NULL,
-  `IdClientePromocao` int(11) DEFAULT NULL,
-  `IdUsuario` int(11) DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `IdClientePromocao` (`IdClientePromocao`),
-  KEY `IdUsuario` (`IdUsuario`),
-  CONSTRAINT `FK_QualificacaoPromocao_Usuario` FOREIGN KEY (`IdUsuario`) REFERENCES `usuario` (`Id`),
-  CONSTRAINT `FK88673F6347ECA03D` FOREIGN KEY (`IdClientePromocao`) REFERENCES `clientepromocao` (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-#
-# Data for table "qualificacaopromocao"
 #
 
