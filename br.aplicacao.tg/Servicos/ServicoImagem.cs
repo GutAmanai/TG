d2cf5a44a4549e66b65af81bd1a48526d2199d77
GutAmanai <gustavo.amanai@gmail.com>
2013-08-30 00:44:59 -0300

    Adicionado css e teste

--- br.aplicacao.tg/Servicos/ServicoImagem.cs
+++ br.aplicacao.tg/Servicos/ServicoImagem.cs
@@ -149,10 +149,10 @@ namespace br.aplicacao.tg.Servicos
 
     public static class UrlExtensions
     {
-        public static string AbsoluteContent(this UrlHelper url, string contentPath)
+        public static string AbsoluteContent(string contentPath)
         {
-            var requestUrl = url.RequestContext.HttpContext.Request.Url;
-            return string.Format("{0}{1}", requestUrl.GetLeftPart(UriPartial.Authority),url.Content(contentPath));
+            var requestUrl = HttpContext.Current.Request.Url;
+            return string.Format("{0}{1}", requestUrl.GetLeftPart(UriPartial.Authority), VirtualPathUtility.ToAbsolute(contentPath));
         }
     }
 }
